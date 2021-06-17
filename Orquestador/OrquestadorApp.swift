ef19f94a4457ce59b8167d48eb135c259179e6bc
Jhoan Medina <medinatello@gmail.com>
2021-06-17 16:55:59 -0400

    Initial Commit

---
+++ Orquestador/OrquestadorApp.swift
@@ -0,0 +1,17 @@
+//
+//  OrquestadorApp.swift
+//  Orquestador
+//
+//  Created by Jhoan Medina on 17-06-21.
+//
+
+import SwiftUI
+
+@main
+struct OrquestadorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
